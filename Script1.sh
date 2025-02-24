407c3064ca8c5ceb9d90188539a507fc73557c20
root <root@ip-172-31-13-136.ap-south-1.compute.internal>
2025-02-24 16:16:25 +0000

    this is shyam's data on repo

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,8 @@
+
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-26 and learning DevOps"
 echo "Hello, Good morning!"
+
 echo "hi my name is pallavi and this is my 1st practical of the devops"
+echo "I am shyam accessing file by using aws"
